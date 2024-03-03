9a5c4226722ad54046d607d72f1380497df6def9
Arun Prajapati <arun.prajapati@cotiviti.com>
2024-03-03 21:00:59 +0545

    Fix test issue

--- frontend/app/__tests__/page.test.js
+++ frontend/app/__tests__/page.test.js
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
-import Home from '../page'
+import Home from '../myday/page'
  
 describe('Page', () => {
   it(' shoul not render a heading', () => {
